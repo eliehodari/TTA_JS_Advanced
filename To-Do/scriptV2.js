9e7936f8c1f90da0c51d3eece1e9f1d13b35e706
eliehodari <eliehodari1@gmail.com>
2025-02-22 12:10:08 -0800

    functioning update button

--- To-Do/scriptV2.js
+++ To-Do/scriptV2.js
@@ -6,20 +6,26 @@ const displayTasks = () => {
     taskDisplay.innerHTML = '';
 
     tasks.forEach((task, index) => {
+        // create the task container
         const taskItem = document.createElement('li');
         taskItem.classList.add('bg-blue-200', 'p-2', 'm-2', 'rounded-lg', 'flex', 'justify-between', 'list-none');
-
+        
+        // create the task title container
         const taskText = document.createElement('span');
         taskText.textContent = task;
         taskItem.appendChild(taskText);
 
+        // create the task links Container
         const taskLinks = document.createElement('div');
         taskLinks.classList.add('task-links');
 
+
+        // create the update and delete buttons
         const updateButton = document.createElement('a');
         updateButton.href = '#';
         updateButton.textContent = 'Update';
-        updateButton.classList.add('text-blue-500' , 'mr-2');
+        updateButton.classList.add('text-blue-500' , 'mr-4');
+        updateButton.addEventListener('click', () => editTask(index));
         taskLinks.appendChild(updateButton);
 
         const deleteButton = document.createElement('a');
@@ -54,6 +60,17 @@ const addTask = () => {
         alert('Please enter a task');
     }
 };
+
+ const editTask = (index) => {
+    const updatedTask = prompt("Update your Tasks", tasks[index]);
+    if(updatedTask && updatedTask.trim()  !== ""){
+        tasks[index] = updatedTask.trim();
+        saveTaskToLocalStorage(); 
+        displayTasks();
+    }else{
+        alert('Please Enter a valid Task');
+    }
+ }
 
 const deleteTask = (index) => {
     if (confirm('Are you sure you want to delete this task?')) {
