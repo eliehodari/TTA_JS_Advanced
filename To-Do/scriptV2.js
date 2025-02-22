ccaa04b7e0847087fa2ede002092d1f4871e68b6
eliehodari <eliehodari1@gmail.com>
2025-02-22 11:50:33 -0800

    chore: added the update button on the layout

--- To-Do/scriptV2.js
+++ To-Do/scriptV2.js
@@ -16,6 +16,12 @@ const displayTasks = () => {
         const taskLinks = document.createElement('div');
         taskLinks.classList.add('task-links');
 
+        const updateButton = document.createElement('a');
+        updateButton.href = '#';
+        updateButton.textContent = 'Update';
+        updateButton.classList.add('text-blue-500' , 'mr-2');
+        taskLinks.appendChild(updateButton);
+
         const deleteButton = document.createElement('a');
         deleteButton.href = '#';
         deleteButton.textContent = 'Delete';
